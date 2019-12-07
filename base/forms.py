4887f87a0cb0908fce828be990b9f4a5422c3751
kz <ma.kozeev@gmail.com>
2019-12-07 17:12:48 +0300

    register works properly

--- base/forms.py
+++ base/forms.py
@@ -4,6 +4,16 @@ from base.models import CustomUser
 
 
 class UserForm(forms.ModelForm):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirmed_password = cleaned_data.get('confirm_password')
+        if password != confirmed_password:
+            raise forms.ValidationError("Passwords don't match", code='invalid')
 
     class Meta:
         model = CustomUser
--- monopoly/UserUrls.py
+++ monopoly/UserUrls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(next_page='registration'), name='logout'),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
 
     path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
