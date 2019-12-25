d25e8f6f73a52b38e65c98d98d24740570ad7d6a
kz <ma.kozeev@gmail.com>
2019-12-25 21:22:30 +0300

    added production dependencies

--- monopoly/settings.py
+++ monopoly/settings.py
@@ -77,8 +77,6 @@ WSGI_APPLICATION = 'monopoly.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER': 'kz1',
